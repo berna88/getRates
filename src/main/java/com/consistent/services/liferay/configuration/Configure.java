dc315fda39029feeee2c8b21a1e2c4fe91c9888f
Bernardo Hernandez <bhernandez@consistent.com.mx>
2019-09-03 01:49:19 -0500

    Borrado de consultas erroneas

--- src/main/java/com/consistent/services/liferay/configuration/Configure.java
+++ src/main/java/com/consistent/services/liferay/configuration/Configure.java
@@ -38,28 +38,17 @@ public class Configure extends Portal{
 				Constants.STRUCTURE_HOTEL_ID =_restConfigurationApi.structureHotelId();
 				log.info("Estructura de hotel localizada="+Constants.STRUCTURE_HOTEL_ID);
 			}
-			else{
-				log.info("No se encontro el identificador de la estructura Hotel");
-				Constants.STRUCTURE_HOTEL_ID = new Long(1516944);
-				log.info("Estructura de hotel no localizada="+Constants.STRUCTURE_HOTEL_ID);
-			}
+		
 			if(_restConfigurationApi.structureRatesId()!=0){
 				Constants.STRUCTURE_RATE_ID =_restConfigurationApi.structureRatesId();
 				log.info("Identificador de rate localizado="+Constants.STRUCTURE_RATE_ID);
 			}
-			else{
-				log.info("No se encontro el identificador de la estructura Rate");
-				Constants.STRUCTURE_RATE_ID = new Long(1516944);
-				log.info("Identificador de rate no localizado="+Constants.STRUCTURE_RATE_ID);
-			}
+			
 			if(_restConfigurationApi.folderId()!=0){
 				Constants.FOLDER_ID =_restConfigurationApi.folderId();
 				log.info("Identificador de folder de hotel localizado="+Constants.FOLDER_ID);
 			}
-			else{
-				Constants.FOLDER_ID = new Long(getFolderId(Constants.NOMBRE_CARPETA_HOTEL));
-				log.info("No se localizo el identificador del folder="+Constants.FOLDER_ID);
-			}
+			
 			} else {
 			System.out.println("The sample DXP REST config object is not yet initialized");
 		}
